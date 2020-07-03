8983557ef918c89654cc0fa067f989985944fa8c
Jesse Kriss <jesse@jklabs.net>
2020-07-03 15:39:20 -0700

    Refactoring to allow for custom handlers

--- index.js
+++ index.js
@@ -43,52 +43,65 @@ function t(template, data) {
   });
 }
 
-export default function stamp(template, data) {
+const removeAttr = (node, attrName) => {
+  node.removeAttribute(attrName);
+};
+
+const keep = (node, keepIt, key) => {
+  keepIt ? removeAttr(node, key) : node.remove();
+};
+
+export const defaultHandlers = {
+  if: ({ node, attribute, dataValue }) => {
+    keep(node, dataValue, attribute.name);
+  },
+  unless: ({ node, attribute, dataValue }) => {
+    keep(node, !dataValue, attribute.name);
+  },
+  html: ({ node, attribute, dataValue }) => {
+    node.innerHTML = dataValue;
+    removeAttr(node, attribute.name);
+  },
+  each: ({ node, attribute, dataValue, handlers }) => {
+    if (dataValue) {
+      const t = document.createElement("template");
+      t.innerHTML = node.innerHTML;
+      node.innerHTML = "";
+      dataValue.forEach((subVal, i) => {
+        const itemData = { "#": i, this: subVal };
+        node.appendChild(stamp(t, itemData, handlers));
+      });
+    } else {
+      node.remove();
+    }
+  },
+};
+
+export default function stamp(template, data, attributeHandlers = {}) {
+  const handlers = Object.assign({}, defaultHandlers, attributeHandlers);
   const clone = template.content.cloneNode(true);
   walk(clone, (node) => {
     if (node.attributes) {
       const toRemove = [];
-      for (let i = 0; i < node.attributes.length; i++) {
-        const attr = node.attributes[i];
+
+      Array.from(node.attributes).forEach((attr) => {
         const key = getKey(attr.value);
         const val = key && getDot(data, key);
 
-        const keep = (b, key) => {
-          b ? toRemove.push(key) : node.remove();
-        };
-
-        if (attr.name === "if") {
-          keep(val, attr.name);
-        } else if (attr.name === "unless") {
-          keep(!val, attr.name);
-        } else if (attr.name === "each") {
-          toRemove.push("each");
-          if (val) {
-            const t = document.createElement("template");
-            t.innerHTML = node.innerHTML;
-            node.innerHTML = "";
-            val.forEach((subVal, i) => {
-              const itemData = { "#": i, this: subVal };
-              node.appendChild(stamp(t, itemData));
-            });
-          } else {
-            node.remove();
-          }
-        } else if (attr.name === "html") {
-          node.innerHTML = val;
-          toRemove.push("html");
+        const fn = handlers[attr.name];
+        if (fn) {
+          fn({ node, attribute: attr, dataValue: val, handlers });
         } else {
           // special handling for event listeners
           const m = attr.name.match(/^on(.*)/);
           if (m) {
             if (val) node.addEventListener(m[1], val);
-            toRemove.push(attr.name);
+            node.removeAttribute(attr.name);
           } else if (val) {
             attr.value = val;
           }
         }
-      }
-      toRemove.map(node.removeAttribute.bind(node));
+      });
     }
     if (node instanceof Text) {
       node.textContent = t(node.textContent, data);
